024f6265f6d413d567a0a509d881f38c9dde5f5c
AhmadSerhall <ahmad.nour.serhal@gmail.com>
2024-03-01 01:30:19 +0200

    trying to fetch cards data from the api

--- User/src/app/app.component.ts
+++ User/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { UserCardComponent } from './user-card/user-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [RouterOutlet,HeaderComponent,UserCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'User';
+  // title = 'User';
+  users: any[] = []; 
 }
--- User/src/app/app.module.ts
+++ User/src/app/app.module.ts
@@ -5,14 +5,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component'; 
-
+import { UserCardComponent } from './user-card/user-card.component';
+import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserCardComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     HttpClientModule,
     AppRoutingModule
   ],
